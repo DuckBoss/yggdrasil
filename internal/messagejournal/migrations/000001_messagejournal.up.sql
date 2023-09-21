19d830e9858df64372582868e4b65360647b9260
Jason Jerome <jajerome@redhat.com>
2023-09-21 14:38:20 -0400

    refactor(journal): updated migrations to use sqlite syntax
    Signed-off-by: Jason Jerome <jajerome@redhat.com>

--- internal/messagejournal/migrations/000001_messagejournal.up.sql
+++ internal/messagejournal/migrations/000001_messagejournal.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS journal (
     id INTEGER NOT NULL PRIMARY KEY,
-    message_id VARCHAR(36) NOT NULL,
+    message_id TEXT NOT NULL,
     sent DATETIME NOT NULL,
-    worker_name VARCHAR(128) NOT NULL,
-    response_to VARCHAR(36),
+    worker_name TEXT NOT NULL,
+    response_to TEXT,
     worker_event INTEGER,
     worker_data TEXT
 );
